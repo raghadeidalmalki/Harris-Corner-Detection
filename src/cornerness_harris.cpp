5794559944327cdca9080ec20030756361311444
Raghad AlMalki <raghadeidalmalki@gmail.com>
2024-08-20 00:36:43 +0300

    Add files via upload

--- src/cornerness_harris.cpp
+++ src/cornerness_harris.cpp
@@ -29,59 +29,57 @@ void cornernessHarris()
 
     // visualize results
     string windowName = "Harris Corner Detector Response Matrix";
-    cv::namedWindow(windowName, 4);
+    cv::namedWindow(windowName, 1);
     cv::imshow(windowName, dst_norm_scaled);
     cv::waitKey(0);
 
-    // STUDENTS NEET TO ENTER THIS CODE (C3.2 Atom 4)
-
-    // Look for prominent corners and instantiate keypoints
+    // locate local maxima in the Harris response matrix
     vector<cv::KeyPoint> keypoints;
-    double maxOverlap = 0.0; // max. permissible overlap between two features in %, used during non-maxima suppression
-    for (size_t j = 0; j < dst_norm.rows; j++)
+    double maxOverlap = 0.0;
+    for (int j = 0; j < dst_norm.rows; j++)
     {
-        for (size_t i = 0; i < dst_norm.cols; i++)
+        for (int i = 0; i < dst_norm.cols; i++)
         {
             int response = (int)dst_norm.at<float>(j, i);
             if (response > minResponse)
-            { // only store points above a threshold
+            {
+                cv::KeyPoint newKeypoint;
+                newKeypoint.pt = cv::Point2f(i, j);
+                newKeypoint.size = 2 * apertureSize;
+                newKeypoint.response = response;
 
-                cv::KeyPoint newKeyPoint;
-                newKeyPoint.pt = cv::Point2f(i, j);
-                newKeyPoint.size = 2 * apertureSize;
-                newKeyPoint.response = response;
-
-                // perform non-maximum suppression (NMS) in local neighbourhood around new key point
-                bool bOverlap = false;
+                // perform a non-maximum suppression (NMS) in a local neighborhood around each maximum
+                bool isOverlapped = false;
                 for (auto it = keypoints.begin(); it != keypoints.end(); ++it)
                 {
-                    double kptOverlap = cv::KeyPoint::overlap(newKeyPoint, *it);
-                    if (kptOverlap > maxOverlap)
+                    double overlap = cv::KeyPoint::overlap(newKeypoint, *it);
+                    if (overlap > maxOverlap)
                     {
-                        bOverlap = true;
-                        if (newKeyPoint.response > (*it).response)
-                        {                      // if overlap is >t AND response is higher for new kpt
-                            *it = newKeyPoint; // replace old key point with new one
-                            break;             // quit loop over keypoints
+                        isOverlapped = true;
+                        if (newKeypoint.response > (*it).response)
+                        {
+                            *it = newKeypoint; // replace the keypoint with a higher response one
+                            break;
                         }
                     }
                 }
-                if (!bOverlap)
-                {                                     // only add new key point if no overlap has been found in previous NMS
-                    keypoints.push_back(newKeyPoint); // store new keypoint in dynamic list
+
+                // add the new keypoint which isn't consider to have overlap with the keypoints already stored in the list
+                if (!isOverlapped)
+                {
+                    keypoints.push_back(newKeypoint);
                 }
             }
-        } // eof loop over cols
-    }     // eof loop over rows
+        }
+    }
 
-    // visualize keypoints
+    // visualize results
     windowName = "Harris Corner Detection Results";
-    cv::namedWindow(windowName, 5);
-    cv::Mat visImage = dst_norm_scaled.clone();
-    cv::drawKeypoints(dst_norm_scaled, keypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-    cv::imshow(windowName, visImage);
+    cv::namedWindow(windowName, 1);
+    cv::Mat imgCorners = dst_norm_scaled.clone();
+    cv::drawKeypoints(dst_norm_scaled, keypoints, imgCorners, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    cv::imshow(windowName, imgCorners);
     cv::waitKey(0);
-    // EOF STUDENT CODE
 }
 
 int main()
